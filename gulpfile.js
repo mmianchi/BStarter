588776a9d29f79dc95545d2b5f6dbc91b41f6b81
Mahshid M <mahshid@Mahshids-MacBook-Pro.local>
2018-04-21 13:25:02 -0700

    staeter package

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,42 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
-const sass =  require('gulp-sass');
+const sass = require('gulp-sass');
 
-// compile Sass and Inject Into Browser
-
+// Compile Sass & Inject Into Browser
 gulp.task('sass', function(){
-  return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
-  .pipe(sass())
-  .pipe(gulp.dest('src/css'))
-  .pipe(browserSync.stream());
+  return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss','src/scss/*.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream());
 });
 
 // Move JS Files to src/js
-
 gulp.task('js', function(){
-  return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-  .pipe(gulp.dest('src/js'))
-  .pipe(browserSync.stream());
+  return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js','node_modules/jquery/dist/jquery.min.js','node_modules/popper.js/dist/umd/popper.min.js'])
+    .pipe(gulp.dest("src/js"))
+    .pipe(browserSync.stream());
 });
 
-// watch Sass and Server
+// Watch Sass & Server
 gulp.task('serve', ['sass'], function(){
   browserSync.init({
     server: "./src"
   });
-  gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'],['sass']);
-  gulp.watch('src/*.html').on('change', browserSync.reload);
+
+  gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+  gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
-// Move fonts folder to src/fonts
+// Move Fonts Folder to src/fonts
+gulp.task('fonts', function(){
+  return gulp.src('node_modules/font-awesome/fonts/*')
+    .pipe(gulp.dest("src/fonts"));
+});
 
-gulp.task('font', function(){
-  return gulp.src('node_modules/@fortawesome/*')
-  .pipe(gulp.dest('src/fonts'));
-})
-
-// Move fonts awesome css to src/css
-
+// Move Font Awesome CSS to src/css
 gulp.task('fa', function(){
-  return gulp.src('node_modules/@fortawesome/fontawesome-free-webfonts/css/fontawesome.css')
-  .pipe(gulp.dest('src/css'));
-})
-
-//
+  return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
+    .pipe(gulp.dest("src/css"));
+});
 
 gulp.task('default', ['js', 'serve', 'fa', 'fonts']);
